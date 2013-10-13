3243d5f3759a51aa5bd79258366af7fd32135638
chris <chris@reinventor.net>
2013-10-14 02:20:01 +0800

    introduce GameUI

--- src/net/reinventor/hex/App.scala
+++ src/net/reinventor/hex/App.scala
@@ -8,34 +8,38 @@ object App extends SimpleSwingApplication {
     title = "Hello world~~"
     minimumSize = new Dimension(800, 600)
 
-    contents = new Component {
-      val hexSize = 20.5
-      val Hex = hexagon(hexSize)
-      def drawHex(g: Graphics2D, coord: (Int, Int)): Unit = {
-        val (q, r) = coord
-        val x = q * 1.5 * hexSize
-        val y = (r + q * .5) * math.sqrt(3) * hexSize
-        val oriTransform = g.getTransform
-        g.translate(x, y)
-        g.drawPolygon(Hex)
-        g.setTransform(oriTransform)
-      }
-
-      override protected def paintComponent(g: Graphics2D): Unit = {
-        g.translate(300, 300)
-        for (hex <- grid.coordinates) {
-          drawHex(g, hex)
-        }
-      }
-    }
-
-    val coords = for {
-      q <- -3 to 3
-      r <- -3 to 3
-    } yield (q, r)
-    val grid = new HexRegion(coords: _*)
+    contents = new GameUI
 
     centerOnScreen()
+  }
+}
+
+class HexRegion(val coordinates: (Int, Int)*)
+
+class GameUI extends Component {
+  val coords = for {
+    q <- -3 to 3
+    r <- -3 to 3
+  } yield (q, r)
+  val grid = new HexRegion(coords: _*)
+
+  val hexSize = 20.5
+  val Hex = hexagon(hexSize)
+  def drawHex(g: Graphics2D, coord: (Int, Int)): Unit = {
+    val (q, r) = coord
+    val x = q * 1.5 * hexSize
+    val y = (r + q * .5) * math.sqrt(3) * hexSize
+    val oriTransform = g.getTransform
+    g.translate(x, y)
+    g.drawPolygon(Hex)
+    g.setTransform(oriTransform)
+  }
+
+  override protected def paintComponent(g: Graphics2D): Unit = {
+    g.translate(300, 300)
+    for (hex <- grid.coordinates) {
+      drawHex(g, hex)
+    }
   }
 
   private def hexagon(size: Double): Polygon = {
@@ -51,5 +55,3 @@ object App extends SimpleSwingApplication {
     new Polygon(x.toArray, y.toArray, 6)
   }
 }
-
-class HexRegion(val coordinates: (Int, Int)*)
