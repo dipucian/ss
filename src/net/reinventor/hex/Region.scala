616216a17ad78cd5f87a92fc6a5c8921ceb67e28
chris <chris@reinventor.net>
2013-10-14 06:06:16 +0800

    refactor Region

--- src/net/reinventor/hex/Region.scala
+++ src/net/reinventor/hex/Region.scala
@@ -4,22 +4,23 @@ trait Region extends Set[Coordinate]
 
 object Region {
   import Coordinate._
+
   def Range(n: Int): Region = {
-    new Region {
-      val cells: Set[Coordinate] = for {
-        x <- (-n to n).toSet[Int]
-        i = math.max(-n, -x-n)
-        j = math.min(n, -x+n)
-        y <- i to j
-        z = -x-y
-      } yield Cube(x, y, z)
+    val cells: Set[Coordinate] = for {
+      x <- (-n to n).toSet[Int]
+      i = math.max(-n, -x-n)
+      j = math.min(n, -x+n)
+      y <- i to j
+      z = -x-y
+    } yield Cube(x, y, z)
+    new RegionImpl(cells)
+  }
 
-      def contains(elem: Coordinate): Boolean = cells.contains(elem)
+  private class RegionImpl(cells: Set[Coordinate]) extends Region {
+    def contains(elem: Coordinate): Boolean = cells.contains(elem)
+    def iterator: Iterator[Coordinate] = cells.iterator
 
-      def iterator: Iterator[Coordinate] = cells.iterator
-
-      def +(elem: Coordinate): Set[Coordinate] = cells.+(elem)
-      def -(elem: Coordinate): Set[Coordinate] = cells.-(elem)
-    }
+    def +(elem: Coordinate): Region = new RegionImpl(cells + elem)
+    def -(elem: Coordinate): Region = new RegionImpl(cells - elem)
   }
 }
